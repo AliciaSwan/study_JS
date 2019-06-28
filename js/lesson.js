466277457df90e4c8613d5655313ba7cf41b1d92
Angelika Popova <ange2002@mail.ru>
2019-06-28 15:58:40 +0100

    Второй урок - простое залдание

--- js/lesson.js
+++ js/lesson.js
@@ -1,12 +1,28 @@
 
 
-var money;
-let income; 
-const addExpenses = []; 
-let deposit; 
-let mission; 
-let period;
+let money = 3200;
+let income = "фриланс"; 
+let addExpenses = 'бензин, ресторан, Аттрационы'; 
+let deposit = true; 
+let mission = 500000; 
+let period = 6;
 
 alert("It's my first lesson");
 
 console.log("It's working! I'm so happy.");
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(income.length);
+
+console.log("Перод " + period + "месяцев." );
+console.log("Цель заработать " + mission + "евро." );
+
+console.log(addExpenses.toLowerCase);
+console.log(addExpenses.split(', '));
+
+
+let budgetDay = money/30;
+console.log(budgetDay, budgetDay%30 );
