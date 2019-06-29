718ad2a91b9e9a0a529e4094bb65646bf04fbe9a
Angelika Popova <ange2002@mail.ru>
2019-06-29 19:49:47 +0100

    Использование reduce

--- js/hard_task.js
+++ js/hard_task.js
@@ -4,16 +4,17 @@ let transform = String(num);
 // разделяем на простые числа
 let numArray = transform.split('');
 // перемножаем числа
-console.log(numArray[0]*numArray[1]*numArray[2]*numArray[3]*numArray[4]*numArray[5]);
-
+//console.log(numArray[0]*numArray[1]*numArray[2]*numArray[3]*numArray[4]*numArray[5]);
+let multip = numArray.reduce((multip,elem) => multip * elem);
+console.log(multip);
 //возводим в степень
-let result = numArray[0]*numArray[1]*numArray[2]*numArray[3]*numArray[4]*numArray[5];
+//let result = numArray[0]*numArray[1]*numArray[2]*numArray[3]*numArray[4]*numArray[5];
 
-result **= 3; 
+multip **= 3; 
 
-console.log(result);
+console.log(multip);
 
-let str = String(result);
+let str = String(multip);
 
 console.log(str.substr(0,2));
 
