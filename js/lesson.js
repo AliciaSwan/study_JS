5ececbe6905f1a4c2280464dc6aa6baea7ffd83e
Angelika Popova <ange2002@mail.ru>
2019-07-01 22:50:58 +0100

    Исправлено объявление переменных

--- js/lesson.js
+++ js/lesson.js
@@ -1,20 +1,20 @@
 'use strict';
- let money = prompt('Ваш месячный доход?');
- let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?');
+ let money = prompt('Ваш месячный доход?'),
+     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую?');
  console.log(addExpenses.split(','));
- let deposit = !!(prompt('Есть ли у вас депозит в банке?'));
- let income = "фриланс"; 
+ let deposit = !!(prompt('Есть ли у вас депозит в банке?')),
+     income = "фриланс"; 
  console.log(money, income, deposit );
- let expenses = prompt("Какие обязательные ежемесячные расходы у вас есть?");
- let costs = prompt("Во сколько это обойдется?"); 
- let expenses2 = prompt("Какие обязательные ежемесячные расходы у вас есть?");
- let costs2 = prompt("Во сколько это обойдется?"); 
+ let expenses = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
+     costs = prompt("Во сколько это обойдется?"), 
+     expenses2 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
+     costs2 = prompt("Во сколько это обойдется?"), 
+     budgetMonth = money - costs - costs2;
 
- let budgetMonth = money - costs - costs2;
  console.log(budgetMonth);
 
- let mission = 500000; 
- let aim = Math.ceil(mission / budgetMonth);
+ let mission = 500000,  
+     aim = Math.ceil(mission / budgetMonth);
  console.log('Цель будет достигнута за ' + aim + ' месяцев');
 
  let budgetDay = Math.floor(budgetMonth/30);
@@ -22,37 +22,10 @@
 
  if (budgetDay>=800){
     console.log("Высокий уровень дохода");
- } else if (budgetDay>300 && budgetDay<800){
+ } else if (budgetDay<800 && budgetDay>300 ){
     console.log("Средний уровень дохода");
- } else if (budgetDay>=0 && budgetDay<=300){
+ } else if (budgetDay<=300 && budgetDay>=0){
     console.log("Низкий уровень дохода");
  } else {
     console.log("Что то пошло не так");
  }
-
-// let money = 3200;
-// let income = "фриланс"; 
-// let addExpenses = 'бензин, ресторан, Аттрационы'; 
-// let deposit = true; 
-// let mission = 500000; 
-// let period = 6;
-
-// alert("It's my first lesson");
-
-// console.log("It's working! I'm so happy.");
-
-// console.log(typeof money);
-// console.log(typeof income);
-// console.log(typeof deposit);
-
-// console.log(income.length);
-
-// console.log("Перод " + period + "месяцев." );
-// console.log("Цель заработать " + mission + "евро." );
-
-// console.log(addExpenses.toLowerCase);
-// console.log(addExpenses.split(', '));
-
-
-// let budgetDay = money/30;
-// console.log(budgetDay, budgetDay%30 );
