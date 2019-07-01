e470ea8af68c152bcf36b1e7752db8922cbd3d29
Angelika Popova <ange2002@mail.ru>
2019-07-01 21:24:37 +0100

    корректировка многомерного массива

--- js/hard_task.js
+++ js/hard_task.js
@@ -13,26 +13,13 @@ switch(lang1){
     break;
     case 'ru': console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
 }
-// let lang3 = 'ru';
-// lang3['ru'] = {'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'};
-// lang3['en'] = {'monday, tuesday, wednesday, thursday, friday, saturday, sunday'};
-// console.log(lang3);
-// lang3 = array(array('ru' => 'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье),
-//               array('en' => 'monday, tuesday, wednesday, thursday, friday, saturday, sunday')
-//               );
 
-//               a = new Array(
-//                 {"attr1":"text1","attr2":"text2"},
-//                 {"attr1":"text3","attr2":"text4"}
-//                 );
-//                 alert( a[0].attr1 );   //Выведет "text1"
-//                 alert( a[1].attr2 );   //Выведет "text4"
 let lang3 = prompt("Выберите язык 'ru' или 'en'");
 console.log( lang3);
-let week = new Array(
-        {"ru":"понедельник, вторник, среда, четверг, пятница, суббота, воскресенье", "fr":"monday, tuesday, wednesday, thursday, friday, saturday, sunday"}
-);   
-console.log( week[0].lang3);            
+let week = 
+        {"ru":["понедельник, вторник, среда, четверг, пятница, суббота, воскресенье"], "fr":["monday, tuesday, wednesday, thursday, friday, saturday, sunday"]
+    };   
+console.log( week[lang3]);            
 
 // вторая часть
 
