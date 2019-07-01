6a315b276a7c85de654a0242d19d2b1615fedf9e
Angelika Popova <ange2002@mail.ru>
2019-07-01 20:02:51 +0100

    Динамическая типизация данных в Javascript.Все об условиях

--- js/hard_task.js
+++ js/hard_task.js
@@ -1,20 +1,43 @@
-let num = 266219;
-// переводим число в строку
-let transform = String(num);
-// разделяем на простые числа
-let numArray = transform.split('');
-// перемножаем числа
-//console.log(numArray[0]*numArray[1]*numArray[2]*numArray[3]*numArray[4]*numArray[5]);
-let multip = numArray.reduce((multip,elem) => multip * elem);
-console.log(multip);
-//возводим в степень
-//let result = numArray[0]*numArray[1]*numArray[2]*numArray[3]*numArray[4]*numArray[5];
+'use strict';
+let lang = 'ru';
 
-multip **= 3; 
+if(lang == 'en'){
+    console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
+} else if (lang === 'ru') {
+    console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'); 
+}
 
-console.log(multip);
+let lang1 = 'en';
+switch(lang1){
+    case 'en': console.log('monday, tuesday, wednesday, thursday, friday, saturday, sunday');
+    break;
+    case 'ru': console.log('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+}
+// let lang3 = 'ru';
+// lang3['ru'] = {'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'};
+// lang3['en'] = {'monday, tuesday, wednesday, thursday, friday, saturday, sunday'};
+// console.log(lang3);
+// lang3 = array(array('ru' => 'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье),
+//               array('en' => 'monday, tuesday, wednesday, thursday, friday, saturday, sunday')
+//               );
 
-let str = String(multip);
+//               a = new Array(
+//                 {"attr1":"text1","attr2":"text2"},
+//                 {"attr1":"text3","attr2":"text4"}
+//                 );
+//                 alert( a[0].attr1 );   //Выведет "text1"
+//                 alert( a[1].attr2 );   //Выведет "text4"
+let lang3 = prompt("Выберите язык 'ru' или 'en'");
+console.log( lang3);
+let week = new Array(
+        {"ru":"понедельник, вторник, среда, четверг, пятница, суббота, воскресенье", "fr":"monday, tuesday, wednesday, thursday, friday, saturday, sunday"}
+);   
+console.log( week[0].lang3);            
 
-console.log(str.substr(0,2));
+// вторая часть
+
+let namePerson = prompt('Введите имя'),
+    result = (namePerson === 'Артём')? 'директор' :
+             (namePerson === 'Максим') ? "преподаватель" : "студент";
+console.log(result);
 
