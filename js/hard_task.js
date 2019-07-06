ff2f470988d2cf0b6e4ab5ab95b46799c0c666d1
Angelika Popova <ange2002@mail.ru>
2019-07-06 16:45:56 +0200

    Дни недели

--- js/hard_task.js
+++ js/hard_task.js
@@ -1,46 +1,19 @@
-'use srict';
+"use strict";
 
-let week = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота' ];
-let date = new Date();
-let numberDay = date.getDay();
-
- for ( let i = 0; i > 7; i++){
-     if (week[i] == 0 || week[i] == 6){
-        document.write(week[i].italics());
-        if (week[i] == date){
-            document.write(week[i].bold());
-        }
-    } else {
-        document.write(week.join("\n"));
+let week = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота' ], 
+    date = new Date(),
+    numberDay = date.getDay();
+ 
+for (let i = 0; i < week.length; i++) { 
+  if (i == numberDay) { 
+    if (week[i] == 'суббота' || week[i] == 'воскресенье') { 
+      document.write('<p style="font-style: italic;"><b>' + week[i] +'</b></p>'); 
+    } else { 
+      document.write('<p>'+ week[i].italics()+'</p>'); 
     }
-}
-
-// }	
-for (let item of week){
-    document.write(week[numberDay].bold());
-}
-
-document.write(week.join ("\n"));
-
-
-
-
- for ( let i = 0; i > 7; i++){
-    if (week[i] == date){
-        document.write(week[i].bold());
-    }
-    if (week[i] == 0 || week[i] == 6){
-        document.write(week[i].italics()); 
-    } else {
-        document.write(week.join("\n"));
-    }
-}
-
-
-
-
-
-
-// var worldString = 'Привет, мир';
-// document.write(worldString.bold());
-// document.write(worldString.italics());
+  } else if (week[i] == 'суббота' || week[i] == 'воскресенье') { 
+    document.write('<p>'+ week[i].italics() +'</p>'); 
+  } else { 
+    document.write('<p>'+ week[i]+'</p>'); 
+  } 
+} 
