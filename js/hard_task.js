12dda4740605dfec77960039583b413848b15cab
Angelika Popova <ange2002@mail.ru>
2019-07-09 12:36:10 +0100

    вывод даты и времени

--- js/hard_task.js
+++ js/hard_task.js
@@ -1,19 +1,42 @@
 'use strict';
+function now() {
+    let date = new Date(),
+        month = date.getMonth()+1,
+        day = date.getDate(),
+        hour = date.getHours(),
+        minutes = date.getMinutes(),
+        seconds = date.getSeconds();
+  
+    let fullDate = hour + ':' + minutes + ':' + seconds +' '+ (day<10 ? '0' : '') + day + "." + (month<10 ? '0' : '') + month + '.'+ date.getFullYear();
+    return fullDate;
+  }
+  console.log(now());
 
-let week = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота' ], 
-    date = new Date(),
-    numberDay = date.getDay();
- 
-for (let i = 0; i < week.length; i++) { 
-  if (i == numberDay) { 
-    if (week[i] == 'суббота' || week[i] == 'воскресенье') { 
-      document.write('<p style="font-style: italic;"><b>' + week[i] +'</b></p>'); 
-    } else { 
-      document.write('<p>'+ week[i].italics()+'</p>'); 
+  function getDateTime() {
+    var now     = new Date(); 
+    var year    = now.getFullYear();
+    var month   = now.getMonth()+1; 
+    var day     = now.getDate();
+    var hour    = now.getHours();
+    var minute  = now.getMinutes();
+    var second  = now.getSeconds(); 
+    if(month.toString().length == 1) {
+         month = '0'+month;
     }
-  } else if (week[i] == 'суббота' || week[i] == 'воскресенье') { 
-    document.writeln('<p>'+ week[i].italics() +'</p>'); 
-  } else { 
-    document.writeln('<p>'+ week[i]+'</p>'); 
-  } 
-} 
+    if(day.toString().length == 1) {
+         day = '0'+day;
+    }   
+    if(hour.toString().length == 1) {
+         hour = '0'+hour;
+    }
+    if(minute.toString().length == 1) {
+         minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+         second = '0'+second;
+    }   
+    var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
+     return dateTime;
+}
+console.log(getDateTime());
+
