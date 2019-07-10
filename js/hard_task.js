00730fd1b98442a922d2966ec9ed0c3adaf07548
Angelika Popova <ange2002@mail.ru>
2019-07-11 00:41:18 +0100

    Подправила функцию

--- js/hard_task.js
+++ js/hard_task.js
@@ -5,11 +5,14 @@ function now() {
         day = date.getDate(),
         hour = date.getHours(),
         minutes = date.getMinutes(),
-        seconds = date.getSeconds();
-  
-    let fullDate = (hour < 10 ? '0' : '') + hour + ':' + (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds + ' '+ (day < 10 ? '0' : '') + day + "." + (month < 10 ? '0' : '') + month + '.' + date.getFullYear();
-    return fullDate;
-    
+        seconds = date.getSeconds(),
+    // let fullDate = (hour < 10 ? '0' : '') + hour + ':' + (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds + ' '+ (day < 10 ? '0' : '') + day + "." + (month < 10 ? '0' : '') + month + '.' + date.getFullYear();
+        fullDate = addZero(hour) + ':' + addZero(minutes) + ':' + addZero(seconds) + ' ' + addZero(day) + '.' + addZero((month) + 1) + '.' + date.getFullYear(); 
+                     
+    function addZero(i) {
+      return (i < 10) ? '0' + i : i;
+    }
+  return fullDate;
   }
 setInterval(function(){
     document.getElementById("clock").innerHTML = now();
