73549731067846e90318385f4eb3513ea608f8e0
Angelika Popova <ange2002@mail.ru>
2019-07-13 14:43:02 +0100

    исправила движение

--- js/script.js
+++ js/script.js
@@ -1,63 +1,51 @@
 "use strict";
 
-function DomElement(selector, height, width, bg, fontSize){
-    this.selector = selector;
+function DomElement( height, width, bg){
     this.height = height;
-    this.width = width;
     this.background = bg;
-    this.fontSize = fontSize;
+    this.width = width;
 }
 
-// ----hard
-
-function Shape(height, width, bg, position){
+function Shape( height, width, bg, position){
     DomElement.apply(this, arguments); 
-    // this.width = width;
-    // this.background = bg;
     this.position = position;
 }
 
 
 DomElement.prototype.shape = function(){
-    let newItem = document.createElement('div');
+    let newItem = document.createElement('div'),
+        left = 0,
+        top = 0;
     newItem.style.cssText = 'height:'+ this.height +'; width:' + this.width +'; background:'+ this.background +'; position:'+ this.position;
     document.addEventListener("DOMContentLoaded", () => {
         document.body.appendChild(newItem);
         document.addEventListener('keydown', function(e){
-           // if(e.keyCode == 39){
-                newItem.style.left = newItem.offsetLeft + (e.keyCode == 37 ? -10 : 10) + 'px';
-           // }else if(e.keyCode == 40){
-                newItem.style.top = newItem.offsetTop + (e.keyCode == 38 ? -10 : 10) + 'px';
-          //  }/*else if(e.keyCode == 37){
-                newItem.style.right = newItem.offsetRight + (e.keyCode == 39 ? 10 : -10) + 'px';
-          //  }else if(e.keyCode == 38){
-                newItem.style.bottom = newItem.offsetBottom + (e.keyCode == 40 ? 10 : -10) + 'px';
-         //   }*/
- //           if(e.keyCode == 37){
-            //     newItem.style.left = '10px';
-            // }else if(e.keyCode == 39){
-            //     newItem.style.right = '10px';
-            // }else if(e.keyCode == 38){
-            //     newItem.style.top = '10px';
-            // }else if(e.keyCode == 40){
-            //     newItem.style.bottom = '10px';
-            // }
+
+           if(e.keyCode == 37){
+                left -= 10;
+                newItem.style.left = left +'px';
+            }else if(e.keyCode == 39){
+                left += 10;
+                newItem.style.left = left +'px';
+            }else if(e.keyCode == 38){
+                top -= 10;
+                newItem.style.top = top +'px';
+            }else if(e.keyCode == 40){
+                top += 10;
+                newItem.style.top = top +'px';
+            }
         });
     });   
 }
 Shape.prototype = Object.create(DomElement.prototype);
 Shape.prototype.constructor = Shape;
 
-let square = new Shape('100px', '100px', 'green', 'absolute');
+let square = new Shape('100px','100px','green', 'absolute');
 square.shape();
 
-window.onkeydown = function (event) {
-    var code = event.keyCode || event.charCode;
-    if (code != 37 && code != 39)
-        return;
-    var prop = document.getElementById('kvadr');
-    prop.style.left = prop.offsetLeft+ (code == 37 ? -10 : 10) + 'px';
-};
+
+
+
 
 
 
