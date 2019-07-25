0c17a408de2ecec6676823f0d7f8a0f7ef39d58a
Angelika Popova <ange2002@mail.ru>
2019-07-25 15:56:56 +0100

    calculator

--- js/script.js
+++ js/script.js
@@ -346,6 +346,71 @@ window.addEventListener('DOMContentLoaded', function(){
 		item.addEventListener('input', () => {
 			item.value = item.value.replace(/\D/g, '');
 		});
-	});
+    });
+    
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcDay = document.querySelector('.calc-day'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+        
+        const countSum = () => {
+            let total = 0,
+            countValue = 1,
+            dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if(calcCount.value > 1){
+                countValue += (calcCount.value - 1) / 10;
+            }
+            if( calcDay.value && calcDay.value < 5){
+                dayValue *= 2;
+            }else if (calcDay.value && calcDay.value < 10){
+                dayValue *= 1.5;
+            }
+
+            if(typeValue && squareValue){
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+            //totalValue.textContent = total;
+            
+            // бегущие цифры
+            let number = 0,
+            interval = null;
+        
+            let runNumber = () => {
+                number ++;
+                if (number < total){
+                    totalValue.textContent = number;
+                } else{
+                    clearInterval(interval);
+                    totalValue.textContent = total;
+                }
+            };
+            interval = setInterval (runNumber, 1);
+
+        };
+
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+            // if(target.matches('.calc-type') || target.matches('.calc-square') ||
+            // target.matches('.calc-day') || target.matches('.calc-count')){
+            //     console.log(1);
+            // }
+            // if(target === calcDay || target === calcSquare || 
+            //     target === calcType || target === calcCount){
+            //         console.log(1);  
+            //     }
+            if(target.matches('select') || target.matches('input')){
+                countSum(price); 
+            }
+        });
+
+    };
+    calc();
 
 }); 
